cb1da2827cdf7249a9c962c319711e751d5fecbf
leo.jjcc <1542123803@qq.com>
2022-04-16 15:03:09 +0800

    project init

--- src/main/java/com/study/mongo/SpringBootMongodbApplication.java
+++ src/main/java/com/study/mongo/SpringBootMongodbApplication.java
@@ -3,11 +3,14 @@ package com.study.mongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Leo
+ */
 @SpringBootApplication
 public class SpringBootMongodbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMongodbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMongodbApplication.class, args);
+    }
 
 }
